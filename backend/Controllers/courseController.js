71a96bef085efd9b95310d5ab279e9564acf0eb2
Sumit6001 <Sumit6001>
2023-11-10 15:52:31 +0530

    code

--- backend/Controllers/courseController.js
+++ backend/Controllers/courseController.js
@@ -71,73 +71,59 @@ const createCourse = async function (req, res) {
   }
 };
 
-module.exports = {createCourse}
+// ===============================================================================
 
-// -------------------------------------------------------------------------------------
+// *************** Get Course API ***************
+const getCourse = async (req, res) => {
+  try {
+    const courses = await CourseModel.find();
+    return res.status(200).send({
+      status: true,
+      msg: "Courses retrieved successfully",
+      data: courses,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, msg: "Internal Server Error" });
+  }
+};
 
-// const updateCourse = async function (req, res) {
-//   try {
-//     let courseId = req.params.id; // Assuming you pass the course ID in the request parameters
-//     let updatedCourseData = req.body; // The updated course data
+// *************** Update Course API ***************
 
-//     // Assuming your courseModel has a method like findByIdAndUpdate to update the course
-//     let updatedCourse = await courseModel.findByIdAndUpdate(
-//       courseId,
-//       updatedCourseData,
-//       { new: true }
-//     );
+const updateCourse = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const updatedCourseData = req.body;
 
-//     if (!updatedCourse) {
-//       return res.status(404).send({ message: "Course not found" });
-//     }
+    // Validation checks...
+    if (!Validation.isValidBody(updatedCourseData)) {
+      return res.status(400).send({
+        status: false,
+        msg: "Invalid data provided for updating the course",
+      });
+    }
 
-//     return res.send({ message: "Course updated successfully", updatedCourse });
-//   } catch (err) {
-//     console.log("server error", err);
-//     return res.status(500).send({ message: "Server error" });
-//   }
-// };
-// const deleteCourse = async function (req, res) {
-//   try {
-//     const courseId = req.params.id; // Assuming the course ID is passed in the URL
+    const updatedCourse = await CourseModel.findByIdAndUpdate(
+      courseId,
+      updatedCourseData,
+      { new: true }
+    );
 
-//     // Use Mongoose to find and remove the course with the given ID.
-//     const deletedCourse = await courseModel.findByIdAndRemove(courseId);
+    if (!updatedCourse) {
+      return res.status(404).send({ status: false, msg: "Course not found" });
+    }
 
-//     if (!deletedCourse) {
-//       return res.status(404).json({ message: "Course not found" });
-//     }
+    return res.status(200).send({
+      status: true,
+      msg: "Course updated successfully",
+      data: updatedCourse,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, msg: "Internal Server Error" });
+  }
+};
 
-//     return res
-//       .status(200)
-//       .json({ message: "Course deleted successfully", data: deletedCourse });
-//   } catch (err) {
-//     console.error("Server error", err);
-//     return res.status(500).json({ message: "Server error" });
-//   }
-// };
-
-// const getCourse = async function (req, res) {
-//   try {
-//     const courseId = req.params.id; // Assuming the course ID is passed in the URL
-
-//     // Use Mongoose to find the course with the given ID.
-//     const foundCourse = await courseModel.findById(courseId);
-
-//     if (!foundCourse) {
-//       return res.status(404).json({ message: "Course not found" });
-//     }
-
-//     return res
-//       .status(200)
-//       .json({ message: "Course retrieved successfully", data: foundCourse });
-//   } catch (err) {
-//     console.error("Server error", err);
-//     return res.status(500).json({ message: "Server error" });
-//   }
-// };
-
-// module.exports = { createCourse, updateCourse, deleteCourse, getCourse };
-// module.exports = {createCourse, updateCourse}
-
-
+module.exports = { createCourse, getCourse, updateCourse };
--- backend/Routes/route.js
+++ backend/Routes/route.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const StudentController = require("../Controllers/studentController");
-const { createCourse } = require("../Controllers/courseController");
+const { createCourse, getCourse , updateCourse} = require("../Controllers/courseController");
 const { verifyToken } = require("../middleware/mid");
 
 router.post("/register", StudentController.createStudent);
 router.post("/login", StudentController.loginStudent);
 
 router.post("/create", verifyToken, createCourse);
-// router.put("/updatecourse", updateCourse);
+router.get("/fetchCourse", getCourse);
+router.put("/update/:id", updateCourse);
 // router.delete("/deletecourse", deleteCourse);
-// router.get("/getcourse", getCourse);
 
 module.exports = router;
